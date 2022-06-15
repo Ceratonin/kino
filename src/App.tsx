6b19e8e815709a87f238771ec24939a2f7cf36b9
Ceratonin <alkash411@gmail.com>
2022-06-15 13:00:02 +0500

    added films and error pages to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./scss/vendors/bootstrap.scss";
 import "./scss/main.scss";
+import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/Main";
 import Films from "./pages/films/Films";
 import Movie from "./pages/movie/Movie";
@@ -8,7 +9,10 @@ import ErrorPage from "./pages/error/ErrorPage";
 function App() {
   return (
     <div className="App">
-      <ErrorPage />
+      <Routes>
+        <Route path="/" element={<Films />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
