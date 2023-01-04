de5ef66555399f4f2e30e8b50edd4806814d9116
Ceratonin <alkash411@gmail.com>
2023-01-04 22:54:21 +0500

    OTHER: Small changes

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://api.themoviedb.org/3/"
 // export const BASE_URL = "https://blooming-waters-34193.herokuapp.com/3/";
 
-// export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
-export const BASE_IMAGE_URL = "https://kinobased.herokuapp.com/";
+export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
+// export const BASE_IMAGE_URL = "https://kinobased.herokuapp.com/";
