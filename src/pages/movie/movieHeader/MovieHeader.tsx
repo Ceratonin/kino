254116b60d4a881c1a4f569026bd823e86e0820d
Ceratonin <alkash411@gmail.com>
2023-01-07 03:03:50 +0500

    style: Add title font size logic

--- src/pages/movie/movieHeader/MovieHeader.tsx
+++ src/pages/movie/movieHeader/MovieHeader.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import movieDataContext from "../../../contexts/movieDataContext";
 
 const MovieHeader = () => {
@@ -6,7 +6,22 @@ const MovieHeader = () => {
     release_date: releaseDate,
     credits,
     title,
+    original_language: originalLanguage,
+    original_title: originalTitle,
   } = useContext(movieDataContext);
+
+  const titleElement = document.querySelector("#title") as HTMLElement;
+
+  useEffect(() => {
+    let titleFontSize = 3;
+    if (title) {
+      while (titleElement.offsetWidth < titleElement.scrollWidth) {
+        titleFontSize -= 0.1;
+        if (titleFontSize === 0.5) break;
+        titleElement.style.fontSize = `${titleFontSize}rem`;
+      }
+    }
+  }, [title, titleElement]);
 
   const getReleaseYear = () => releaseDate.split("-")[0];
 
@@ -16,17 +31,28 @@ const MovieHeader = () => {
     return director ? director.name : "No Information";
   };
 
+  const checkOriginalTitleLang = () => {
+    return originalLanguage === "en" ? "" : originalTitle;
+  };
+
   return (
-    <div className="movie_header">
-      <div className={`movie_title ${title.length > 25 ? "text-small" : ""}`}>
+    <div className="movie-page__header">
+      <p id="title" className="movie-page__title">
         {title}
-      </div>
-      <div className="side_info">
+      </p>
+      <div className="movie-page__side-info">
         <p>{getReleaseYear()}</p>
-        <div className="movie_shot_by">
+        <span className="movie-page__shot-by">
           <p>Shot by:&ensp;</p>
           <p>{findDirector()}</p>
-        </div>
+        </span>
+        <p
+          className={`movie-page__original-title ${
+            originalTitle.length > 45 ? "text-small" : ""
+          }`}
+        >
+          {checkOriginalTitleLang()}
+        </p>
       </div>
     </div>
   );
